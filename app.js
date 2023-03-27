e24cd335d4299836f7f7d2c82978fbd39b7d93d5
julien belinga <julienbelinga29@gmail.com>
2023-03-27 14:14:27 +0200

    Renew project organization and add Jest tests

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const AppError = require("./utils/appError");
+const AppError = require("./main/utils/appError");
 require("dotenv").config();
 const app = express();
 
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-const authRoutes = require("./routes/authRoutes.routes");
-const productRoutes = require("./routes/productRoutes.routes");
+const authRoutes = require("./main/routes/authRoutes.routes");
+const productRoutes = require("./main/routes/productRoutes.routes");
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/products", productRoutes);
@@ -39,7 +39,7 @@ app.all("*", (req, res, next) => {
 });
 
 // Error handling middleware
-const errorHandler = require("./utils/errorHandler");
+const errorHandler = require("./main/utils/errorHandler");
 app.use(errorHandler);
 
 // Start the server
