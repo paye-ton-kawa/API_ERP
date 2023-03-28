af1c208cd9f935e21bcbb22e1c0d8abf421ec9cc
julien belinga <julienbelinga29@gmail.com>
2023-03-28 13:57:01 +0200

    resolve conflict

--- main/controllers/productController.js
+++ main/controllers/productController.js
@@ -2,25 +2,13 @@ const fs = require("fs");
 
 // controller to get all products
 exports.getProducts = (req, res) => {
-<<<<<<< HEAD
-	const products = JSON.parse(
-		fs.readFileSync(pathResolver.join("data/products.json"))
-	);
-=======
 	const products = JSON.parse(fs.readFileSync("../data/products.json"));
->>>>>>> 109c2b2a45d65e937918e0cddc7fb6dfe556269a
 	res.json(products);
 };
 
 // controller to get a product by its id
 exports.getProductById = (req, res) => {
-<<<<<<< HEAD
-	const products = JSON.parse(
-		fs.readFileSync(pathResolver.join("data/products.json"))
-	);
-=======
 	const products = JSON.parse(fs.readFileSync("../data/products.json"));
->>>>>>> 109c2b2a45d65e937918e0cddc7fb6dfe556269a
 	const { id } = req.params;
 	const product = products.find((p) => p.id === id);
 	if (product) {
